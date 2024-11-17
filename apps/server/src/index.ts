576e453165b28769782ea7aaff994e49fb951154
johnny-woodtke <johnnywoodtke@gmail.com>
2024-11-17 13:28:41 -0500

    best docs ever

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -5,13 +5,42 @@ import { cors } from "@elysiajs/cors";
 const app = new Elysia()
   .use(swagger())
   .use(cors())
-  .get("/", () => "Hello Elysia")
-  .get("/id/:id", ({ params }) => params.id)
+  .get("/", () => "Hello Elysia", {
+    detail: {
+      summary: "Hello Elysia",
+      description: "This is a simple hello world endpoint",
+    },
+    response: {
+      200: t.String(),
+    },
+  })
+  .get("/id/:id", ({ params }) => params.id, {
+    params: t.Object({
+      id: t.Number(),
+    }),
+    detail: {
+      summary: "Get ID",
+      description: "This is a simple get id endpoint",
+    },
+    response: {
+      200: t.Number(),
+    },
+  })
   .post("/mirror", ({ body }) => body, {
     body: t.Object({
       id: t.Number(),
       name: t.String(),
     }),
+    detail: {
+      summary: "Mirror",
+      description: "This is a simple mirror endpoint",
+    },
+    response: {
+      200: t.Object({
+        id: t.Number(),
+        name: t.String(),
+      }),
+    },
   })
   .listen(3000);
 
