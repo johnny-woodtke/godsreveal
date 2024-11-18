fe8725d0c1df73c1db4b9f9b5ec7d4fc8dd063f6
johnny-woodtke <johnnywoodtke@gmail.com>
2024-11-18 16:49:32 -0500

    slate theme fr

--- apps/web/lib/eden.ts
+++ apps/web/lib/eden.ts
@@ -1,11 +1,9 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "@godsreveal/server";
 
-const envVarName = "NEXT_PUBLIC_SERVER_URL";
-
-const serverUrl = process.env[envVarName];
+const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
 if (!serverUrl) {
-  throw new Error(`${envVarName} is not set`);
+  throw new Error("NEXT_PUBLIC_SERVER_URL is not set");
 }
 
 export const client = treaty<App>(serverUrl);
