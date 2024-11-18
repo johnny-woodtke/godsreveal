b6b873bce891753672f1e0e3ae2cfec147fe3c1a
johnny-woodtke <johnnywoodtke@gmail.com>
2024-11-18 16:41:27 -0500

    small clean

--- apps/web/lib/eden.ts
+++ apps/web/lib/eden.ts
@@ -1,9 +1,11 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "@godsreveal/server";
 
-const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
+const envVarName = "NEXT_PUBLIC_SERVER_URL";
+
+const serverUrl = process.env[envVarName];
 if (!serverUrl) {
-  throw new Error("NEXT_PUBLIC_SERVER_URL is not set");
+  throw new Error(`${envVarName} is not set`);
 }
 
 export const client = treaty<App>(serverUrl);
