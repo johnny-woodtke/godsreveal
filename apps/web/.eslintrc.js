3b1d8dc8b1197e0c8524b4f39afeec78b9c791cd
johnny-woodtke <johnnywoodtke@gmail.com>
2024-11-18 20:59:49 -0500

    tanstack integration

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,7 +1,10 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@godsreveal/eslint-config/next.js"],
+  extends: [
+    "@godsreveal/eslint-config/next.js",
+    "plugin:@tanstack/query/recommended",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
