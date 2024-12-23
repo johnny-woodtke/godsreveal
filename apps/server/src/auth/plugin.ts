bd490a084e54de07809038b78778d3e64d40396a
johnny-woodtke <johnnywoodtke@gmail.com>
2024-12-23 10:30:10 -0500

    auth check endpoint

--- apps/server/src/auth/plugin.ts
+++ apps/server/src/auth/plugin.ts
@@ -1,32 +1,44 @@
-import Elysia from "elysia";
+import Elysia, { t } from "elysia";
+
+import { Tag } from "@/constants";
 
 import { getAuthCookieNameOrThrow, getAuthCookieSecretOrThrow } from "./utils";
 
-export default new Elysia().guard({
-  as: "scoped",
-  beforeHandle: ({ cookie, error }) => {
-    // get auth cookie name
-    let authCookieName: string;
-    try {
-      authCookieName = getAuthCookieNameOrThrow();
-    } catch (e) {
-      console.error("Error getting auth cookie name:", e);
-      return error(500, "Internal server error");
-    }
+export default new Elysia()
+  .guard({
+    as: "scoped",
+    beforeHandle: ({ cookie, error }) => {
+      // get auth cookie name
+      let authCookieName: string;
+      try {
+        authCookieName = getAuthCookieNameOrThrow();
+      } catch (e) {
+        console.error("Error getting auth cookie name:", e);
+        return error(500, "Internal server error");
+      }
 
-    // get auth cookie secret
-    let authCookieSecret: string;
-    try {
-      authCookieSecret = getAuthCookieSecretOrThrow();
-    } catch (e) {
-      console.error("Error getting auth cookie secret:", e);
-      return error(500, "Internal server error");
-    }
+      // get auth cookie secret
+      let authCookieSecret: string;
+      try {
+        authCookieSecret = getAuthCookieSecretOrThrow();
+      } catch (e) {
+        console.error("Error getting auth cookie secret:", e);
+        return error(500, "Internal server error");
+      }
 
-    // get client auth cookie value and compare with secret
-    const clientAuthCookie = cookie[authCookieName]?.value;
-    if (clientAuthCookie !== authCookieSecret) {
-      return error(401, "Unauthorized");
-    }
-  },
-});
+      // get client auth cookie value and compare with secret
+      const clientAuthCookie = cookie[authCookieName]?.value;
+      if (clientAuthCookie !== authCookieSecret) {
+        return error(401, "Unauthorized");
+      }
+    },
+  })
+  .get("/auth", () => "OK", {
+    params: t.Undefined(),
+    response: t.String(),
+    detail: {
+      summary: "Auth check",
+      description: "Check if the request is authenticated",
+      tags: [Tag.DEFAULT],
+    },
+  });
