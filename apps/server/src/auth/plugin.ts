fc4c32e35d541c67db75ec562f779edb83b26c03
johnny-woodtke <johnnywoodtke@gmail.com>
2024-12-24 12:42:50 -0500

    remove auth cookies after all requests

--- apps/server/src/auth/plugin.ts
+++ apps/server/src/auth/plugin.ts
@@ -36,6 +36,22 @@ export default new Elysia()
       }
     },
   })
+  .guard({
+    as: "global",
+    afterHandle: ({ cookie, error }) => {
+      // get auth cookie name
+      let authCookieName: string;
+      try {
+        authCookieName = getAuthCookieNameOrThrow();
+      } catch (e) {
+        console.error("Error getting auth cookie name:", e);
+        return error(500, "Internal server error");
+      }
+
+      // remove auth cookie
+      cookie[authCookieName]?.remove();
+    },
+  })
   .get("/auth", () => "OK", {
     params: t.Undefined(),
     response: t.String(),
