4274fe8a2e1004663095e6672583023bb7293c4d
johnny-woodtke <johnnywoodtke@gmail.com>
2024-12-27 15:12:27 -0500

    clean up proxy

--- apps/reverse-proxy/src/index.ts
+++ apps/reverse-proxy/src/index.ts
@@ -12,73 +12,89 @@
  */
 export default {
 	async fetch(request, env, ctx) {
-		// log original request
-		logRequest("INCOMING REQUEST", request);
-
 		// ensure origin is valid
-		const origin = request.headers.get("x-forwarded-host");
-		const validOrigins = env.VALID_ORIGINS.split(",");
-		if (!origin || !validOrigins.includes(origin)) {
+		const validOrigin = validateOrigin(request, env);
+		if (!validOrigin) {
 			return new Response("Unauthorized", { status: 401 });
 		}
 
-		// add auth headers
-		const requestHeaders = new Headers(request.headers);
-		requestHeaders.set("x-godsreveal-auth", env.AUTH_HEADER_SECRET);
-		requestHeaders.set("referer", env.REFERER);
-
-		// update request URL
-		const url = new URL(request.url);
-		const newUrl = new URL(url.pathname + url.search, env.SERVER_URL);
-
 		// create new request
-		const newRequest = new Request(newUrl, {
+		const newRequest = new Request(getServerUrl(new URL(request.url), env), {
 			method: request.method,
-			headers: requestHeaders,
+			headers: addAuthHeaders(request.headers, env),
 			body: request.body,
 		});
-
-		// log request to forward
-		logRequest("FORWARDING REQUEST", newRequest);
 
 		// forward request to server and get response
 		const response = await fetch(newRequest);
 
-		// log response
-		logResponse("RESPONSE", response);
-
-		// remove auth headers
-		const responseHeaders = new Headers(response.headers);
-		responseHeaders.delete("x-forwarded-host");
-		responseHeaders.delete("x-godsreveal-auth");
-		responseHeaders.delete("referer");
-
 		// copy response
 		const newResponse = new Response(response.body, {
 			...response,
-			headers: responseHeaders,
+			headers: filterAuthHeaders(response.headers),
 		});
-
-		// log response to return
-		logResponse("RETURNING RESPONSE", newResponse);
 
 		// return response to client
 		return newResponse;
 	},
 } satisfies ExportedHandler<Env>;
 
-function logRequest(message: string, request: Request) {
-	console.log(message, "\n", {
-		method: request.method,
-		url: request.url,
-		headers: request.headers,
-	});
+function validateOrigin(request: Request, env: Env): boolean {
+	const origin = request.headers.get("x-forwarded-host");
+	const validOrigins = env.VALID_ORIGINS.split(",");
+	return !!origin && validOrigins.includes(origin);
 }
 
-function logResponse(message: string, response: Response) {
-	console.log(message, "\n", {
-		status: response.status,
-		statusText: response.statusText,
-		headers: response.headers,
-	});
+function getServerUrl(url: URL, env: Env): URL {
+	return new URL(url.pathname + url.search, env.SERVER_URL);
+}
+
+function addAuthHeaders(headers: Headers, env: Env): Headers {
+	// copy headers
+	const newHeaders = new Headers(headers);
+
+	// add auth headers
+	newHeaders.set("x-godsreveal-auth", env.AUTH_HEADER_SECRET);
+	newHeaders.set("referer", env.REFERER);
+
+	// return new headers
+	return newHeaders;
+}
+
+const AUTH_HEADERS: Record<string, boolean> = {
+	"x-godsreveal-auth": true,
+	referer: true,
+	"x-forwarded-host": true,
+};
+
+const CORS_HEADERS = [
+	"access-control-allow-headers",
+	"access-control-expose-headers",
+];
+
+function filterAuthHeaders(headers: Headers): Headers {
+	// copy headers
+	const responseHeaders = new Headers(headers);
+
+	// remove auth headers
+	for (const header of Object.keys(AUTH_HEADERS)) {
+		responseHeaders.delete(header);
+	}
+
+	// filter auth headers from CORS headers
+	for (const header of CORS_HEADERS) {
+		const value = responseHeaders.get(header);
+		if (value) {
+			responseHeaders.set(
+				header,
+				value
+					.split(", ")
+					.filter((header) => !AUTH_HEADERS[header])
+					.join(", "),
+			);
+		}
+	}
+
+	// return filtered headers
+	return responseHeaders;
 }
